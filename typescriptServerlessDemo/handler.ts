259fe5a23e41b7ca5c46cd63a023638a40095c9d
Glen Adamson <glenadamson@Glens-MacBook-Air.local>
2020-03-09 21:03:32 +0000

    setup

---
+++ typescriptServerlessDemo/handler.ts
@@ -0,0 +1,9 @@
+import HelloWorldService from "./helloworld";
+
+const helloWorldService = new HelloWorldService();
+
+export const {
+    helloworld,
+    hello
+} = helloWorldService;
+
---
+++ typescriptServerlessDemo/helloworld.ts
@@ -0,0 +1,25 @@
+import { Handler } from 'aws-lambda';
+import 'source-map-support/register';
+
+export default class HelloWorldService {
+
+public helloworld: Handler = async (event, _context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event,
+    }, null, 2),
+  };
+}
+
+public hello: Handler = async (event, _context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event,
+    }, null, 2),
+  };
+}
+}
